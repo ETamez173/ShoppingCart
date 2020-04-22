30006d1871f03ab46a34f57839c8c26b8112bb18
ETamez9173 <elitamez17@gmail.com>
2020-04-22 17:14:03 -0500

    first commit

---
+++ ShoppingCart/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        // Adam explains this ShoppingCart intro part 4 at 14 min point
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+        public DbSet<ShoppingItem> ShoppingItem { get; set; }
+
+    }
+}
